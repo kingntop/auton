dadae4cdf95069602d1523c56a67b54badbe81d8
limyoungmin <kingntop@naver.com>
2022-08-11 19:33:49 +0900

    11

--- tests/auton.spec.ts
+++ tests/auton.spec.ts
@@ -3,6 +3,18 @@ import {
   expect
 } from '@playwright/test';
 
+import {
+  replaceK,
+  yesterday,
+} from "./common/utils";
+
+// Apex 연동
+import {
+  getUid,
+  postApex,
+  getUrlList
+} from "./common/apex";
+
 
 test.describe('Auton', async () => {
 
@@ -28,6 +40,16 @@ test.describe('Auton', async () => {
 
   test('회사소개', async ({page}) => {
 
+    const urlList = await getUrlList();
+    console.log(urlList)
+
+    for (const urlJson of urlList) {
+      const url = urlJson.url;
+      
+      await page.goto(url);
+      await page.screenshot({path: './screenshot/' + replaceK(url) + '.png'});
+
+    }
     await page.goto('https://auton.kr/company/about');
 
 
--- tests/common/apex.ts
+++ tests/common/apex.ts
@@ -3,50 +3,32 @@ import axios, {
     AxiosStatic
 } from 'axios';
 
-import {
-    yesterday,
-    yesterdayDB
-} from "./utils";
+// import {
+//     yesterday,
+//     yesterdayDB
+// } from "./utils";
 
-import fs from 'fs';
-import FormData from 'form-data';
+// import fs from 'fs';
+// import FormData from 'form-data';
 
-// https://gb9fb258fe17506-apexdb.adb.ap-seoul-1.oraclecloudapps.com/ords/twright/v1/twright/logs/:rid
+// // https://gb9fb258fe17506-apexdb.adb.ap-seoul-1.oraclecloudapps.com/ords/twright/v1/twright/logs/:rid
 
-const auth_key = '766614B7C9A901198F2F5630349ADB7A9DAFB63976AF64DBB8A775D3BCCBDDB1'
-const DEV = 'https://g575dfbc1dbf538-playwright.adb.ap-seoul-1.oraclecloudapps.com/ords/playwright/'
-const post_url = DEV + 'v1/twright/logs/'
-const get_url  = DEV + 'v1/twright/tests/'
-const get_all_url = DEV + 'twright/v1/twright/tests' 
-
-
-const image_url = 'images/images/';
-const json_url = 'jons//jsons/'
+// const auth_key = '766614B7C9A901198F2F5630349ADB7A9DAFB63976AF64DBB8A775D3BCCBDDB1'
+// const DEV = 'https://g575dfbc1dbf538-playwright.adb.ap-seoul-1.oraclecloudapps.com/ords/playwright/'
+// const post_url = DEV + 'v1/twright/logs/'
+// const get_url  = DEV + 'v1/twright/tests/'
+// const get_all_url = DEV + 'twright/v1/twright/tests' 
 
 
-/**
- * Test UID 정보
- * @param {string} uid uid
- * @returns {user[]} id, pwd array
- */
-async function getUid(uid: string): Promise < any[] > {
-    let users: any[] = [];
-    // userinfo_url = get_url + payment
-    console.log(get_url + uid)
-    const response:any =  await axios.get(get_url + uid, {
-        // headers: {
-        //     Authorization: auth_key 
-        // }
-    })
-    // console.log(response.data)
-    return response.data
-}
+// const image_url = 'images/images/';
+// const json_url = 'jons//jsons/'
 
-async function getUidAll(): Promise < any[] > {
+
+
+async function getUrlList(): Promise < any[] > {
     let users: any[] = [];
 
-    console.log(get_all_url)
-    const response:any =  await axios.get('https://gb9fb258fe17506-apexdb.adb.ap-seoul-1.oraclecloudapps.com/ords/twright/v1/twright/tests', {
+    const response:any =  await axios.get('https://g575dfbc1dbf538-playwright.adb.ap-seoul-1.oraclecloudapps.com/ords/playwright/lists/lists/', {
         // headers: {
         //     Authorization: auth_key 
         // }
@@ -55,82 +37,81 @@ async function getUidAll(): Promise < any[] > {
     return response.data.items
 }
 
-async function postApex(rid : String, upJson: any):Promise < boolean > {
-    const request_config = {
-        headers: {
-            "Content-Type": 'application/json',
-            "Authorization": auth_key,
-        },
-        maxContentLength: Infinity,
-        maxBodyLength: Infinity,
-        data: upJson
-    };
-    const response = await axios.post(post_url + rid, upJson, request_config);
-    console.log(post_url, upJson)
-    try {
-        if (response.status === 200) { // response - object, eg { status: 200, message: 'OK' }
-            console.log(response.data);
-            return true;
-        }
-        return false;
-    } catch (err) {
-        console.error(err)
-        return false;
-    }
-}
+// async function postApex(rid : String, upJson: any):Promise < boolean > {
+//     const request_config = {
+//         headers: {
+//             "Content-Type": 'application/json',
+//             "Authorization": auth_key,
+//         },
+//         maxContentLength: Infinity,
+//         maxBodyLength: Infinity,
+//         data: upJson
+//     };
+//     const response = await axios.post(post_url + rid, upJson, request_config);
+//     console.log(post_url, upJson)
+//     try {
+//         if (response.status === 200) { // response - object, eg { status: 200, message: 'OK' }
+//             console.log(response.data);
+//             return true;
+//         }
+//         return false;
+//     } catch (err) {
+//         console.error(err)
+//         return false;
+//     }
+// }
 
-async function upload_video( fileType: String, fileName: String, fileLocation) {
+// async function upload_video( fileType: String, fileName: String, fileLocation) {
 
-  const fs=require('fs'),lazy = require('lazy');
-  const form_data = new FormData();
+//   const fs=require('fs'),lazy = require('lazy');
+//   const form_data = new FormData();
 
-  fs.readdir('./videos',function(err,files){    //Get a listing of all the files in the dir
-    if (err) throw err;
-    files.forEach(function(file){
-        console.log(page_id, file);
-        form_data.append('file', fs.createReadStream('videos/' + file));
-        const request_config = {
-          headers: {
-            "filename": file,
-            "image_type": 'image/webp',
-            "page_id":page_id,
-            "serial_id": serial_id,
-            "Content-Type": "multipart/form-data"
-          },
-          maxContentLength: Infinity,
-          maxBodyLength: Infinity,
-          data: form_data
-        };
-        return axios
-          .post(DEV+upload_image, form_data, request_config); 
-    });
-  })
+//   fs.readdir('./videos',function(err,files){    //Get a listing of all the files in the dir
+//     if (err) throw err;
+//     files.forEach(function(file){
+//         console.log(page_id, file);
+//         form_data.append('file', fs.createReadStream('videos/' + file));
+//         const request_config = {
+//           headers: {
+//             "filename": file,
+//             "image_type": 'image/webp',
+//             "page_id":page_id,
+//             "serial_id": serial_id,
+//             "Content-Type": "multipart/form-data"
+//           },
+//           maxContentLength: Infinity,
+//           maxBodyLength: Infinity,
+//           data: form_data
+//         };
+//         return axios
+//           .post(DEV+upload_image, form_data, request_config); 
+//     });
+//   })
 
-}
-async function upload_image(url: String, fileType: String, fileName: String, fileLocation) {
-  const form_data = new FormData();
-  form_data.append('file', fs.createReadStream(fileLocation));
+// }
+// async function upload_image(url: String, fileType: String, fileName: String, fileLocation) {
+//   const form_data = new FormData();
+//   form_data.append('file', fs.createReadStream(fileLocation));
 
-  const request_config = {
-    headers: {
-      "filename": fileName,
-      "image_type": fileType,
-      "page_id":page_id,
-      "loadtime" : endTime-startTime-500,
-      "serial_id": serial_id,
-      "Content-Type": "multipart/form-data"
-    },
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-    data: form_data
-  };
-  return axios
-    .post(DEV+upload_image, form_data, request_config);
-}
+//   const request_config = {
+//     headers: {
+//       "filename": fileName,
+//       "image_type": fileType,
+//       "page_id":page_id,
+//       "loadtime" : endTime-startTime-500,
+//       "serial_id": serial_id,
+//       "Content-Type": "multipart/form-data"
+//     },
+//     maxContentLength: Infinity,
+//     maxBodyLength: Infinity,
+//     data: form_data
+//   };
+//   return axios
+//     .post(DEV+upload_image, form_data, request_config);
+// }
 
 
 export {
-    getUid,
-    postApex,
-    getUidAll
+    // postApex,
+    getUrlList
 }
