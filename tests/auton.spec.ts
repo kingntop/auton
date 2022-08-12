a788e57935592038a3484e5c5ca9c1f95c1e8e44
limyoungmin <kingntop@naver.com>
2022-08-12 22:36:43 +0900

    111

--- tests/auton.spec.ts
+++ tests/auton.spec.ts
@@ -18,23 +18,17 @@ import {
   postApex
 } from "./common/apex";
 
-
 import fs from 'fs';
-
-
 test('TestCase', async ({
 
 }) => {
   const urlList = await getUrlList();
+
+  let  videoUrl = []
   
   for (let i = 0; i < urlList.length; i++) {
-
-    const browser = await firefox.launch({});
-    const context = await browser.newContext({
-      recordVideo: {
-        dir: `./video/${urlList[i].TEST_ID}/${today}`
-      }
-    });
+    const browser = await firefox.launch({ });
+    const context = await browser.newContext({});
     await context.tracing.start({
       screenshots: true,
       snapshots: true
@@ -56,8 +50,6 @@ test('TestCase', async ({
       await context.tracing.stop({
         path: `./trace/${urlList[i].TEST_ID}/${today}.zip`
       });
-      const files = fs.readdirSync(`./video/${urlList[i].TEST_ID}/${today}`);
-      console.log(files[0])
       const upJson = {
         id: urlList[i].TEST_ID,
         url: url,
@@ -65,12 +57,9 @@ test('TestCase', async ({
         cdate: today,
         success: 'Y',
         screenshot: `/screenshot/${urlList[i].TEST_ID}/${today}.png`,
-        video: `/video/${urlList[i].TEST_ID}/${today}/${files[0]}`,
-        trace: `/trace/${urlList[i].TEST_ID}/${today}.zip`
+        traces: `/trace/${urlList[i].TEST_ID}/${today}.zip`
       }
-
-      const result = await postApex(upJson);
-      console.log(upJson)
+      videoUrl.push(upJson)
     } catch (err) {
       const upJson = {
         id: urlList[i].TEST_ID,
@@ -79,8 +68,43 @@ test('TestCase', async ({
         success: 'N'
       }
       console.log(upJson)
+      const result = await postApex(upJson);
     }
     await browser.close();
+  }
+  
+  for (let i = 0; i < videoUrl.length; i++) {
 
+    const browser = await chromium.launch({ });
+    const context = await browser.newContext({
+      recordVideo: {
+        dir: `./video/${urlList[i].TEST_ID}/${today}`
+      }
+    });
+
+    try {
+      const page = await context.newPage();
+      const url = videoUrl[i].url;
+      const startTime = new Date();
+      await page.goto(url, {
+        waitUntil: 'networkidle',
+        timeout: 3000
+      });
+      const files = fs.readdirSync(`./video/${urlList[i].TEST_ID}/${today}`);
+      console.log(files[0])
+      const upJson = {
+        id: videoUrl[i].id,
+        url: videoUrl[i].url,
+        elapsed: videoUrl[i].elapsed,
+        cdate: videoUrl[i].cdate,
+        success: videoUrl[i].success,
+        screenshot: videoUrl[i].screenshot,
+        video: `/video/${videoUrl[i].id}/${today}/${files[0]}`,
+        traces: videoUrl[i].traces
+      }
+      const result = await postApex(upJson);
+      console.log(upJson)
+    } catch (err) { }
+    await browser.close();
   }
 })
--- tests/common/utils.ts
+++ tests/common/utils.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 
 const yesterday = dayjs().subtract(1, "d").format("YYYY-MM-DD");
 const yesterdayDB = dayjs().subtract(1, "d").format("YYYYMMDD");
-const today = dayjs().format("YYYYMMDDHHmmss");
+const today = dayjs().subtract(0, "d").format("YYYYMMDDHHmmss");
 
 function replaceK(data:string):String {
     return data.replace('ID. ', '').replace('개', '').replace('건', '').replace(/,/gi, '').replace('원', '').trim();
