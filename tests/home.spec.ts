ce677d51908ad3f6e6e6fdaf9276905338259ea9
limyoungmin <kingntop@naver.com>
2022-08-19 15:43:16 +0900

    11

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -29,30 +29,35 @@ test('test', async ({}) => {
     console.log(uidInfo.uid);
     const screenInfo = await getUid(uidInfo.uid);
     const screens = screenInfo.screen;
-    for (const screen of screens) {
-      const browser = await chromium.launch();
-      const device = devices[screen.description]
-      const context = await browser.newContext({
-        ...device
-      });
-      const page = await context.newPage()
-      let start_mi = Date.now();
-      await page.goto(uidInfo.url, {
-        waitUntil: 'domcontentloaded'
-      });
-      await page.screenshot({
-        path: 'public/home/' + uidInfo.uid + '/' + screen.code + '.png',
-        fullPage: true
-      });
-      let end_mi = Date.now();
-      console.log(start_mi, end_mi, end_mi - start_mi);
-      const apexJson = {
-        code: screen.code,
-        uid: uidInfo.uid,
-        status: 0,
-        elapsed: end_mi - start_mi
+    const browserTypes = [chromium, webkit];
+    for (const browserType of browserTypes) {
+      for (const screen of screens) {
+        const browser = await chromium.launch();
+        const device = devices[screen.description]
+        const context = await browser.newContext({
+          ...device
+        });
+        if (browserType.name() == device.defaultBrowserType) {
+          const page = await context.newPage()
+          let start_mi = Date.now();
+          await page.goto(uidInfo.url, {
+            waitUntil: 'domcontentloaded'
+          });
+          await page.screenshot({
+            path: 'public/home/' + uidInfo.uid + '/' + screen.code + '.png',
+            // fullPage: true
+          });
+          let end_mi = Date.now();
+          console.log(start_mi, end_mi, end_mi - start_mi);
+          const apexJson = {
+            code: screen.code,
+            uid: uidInfo.uid,
+            status: 0,
+            elapsed: end_mi - start_mi
+          }
+          Json.push(apexJson)
+        }
       }
-      Json.push(apexJson)
     }
     console.log(uidInfo.uid);
     await postApexScreen(uidInfo.uid, Json)
